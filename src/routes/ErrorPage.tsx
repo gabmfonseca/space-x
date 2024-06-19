f0bc1e6f056d68cb9cebb398b1ecfa697175b31e
gabmfonseca <gabriela.fonseca@akqa.com>
2024-06-19 06:22:15 +0200

    add error page

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,15 +1,23 @@
-import { useRouteError } from 'react-router-dom';
+import { Button, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
+import FallbackPatch from '../fallbackPatch.png';
 
 const ErrorPage = () => {
-  const error = useRouteError();
-  console.error(error);
-
   return (
     <Layout>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>{/* <i>{error.statusText || error.message}</i> */}</p>
+      <Stack textAlign="center" alignItems="center">
+        <Image src={FallbackPatch} maxW="300px" />
+        <Stack spacing="4">
+          <Heading size="4xl">404</Heading>
+          <Text fontSize="xl">We can't find the page you're looking for.</Text>
+          <Link to="/">
+            <Button size="lg" variant="outline">
+              Go back home
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
     </Layout>
   );
 };
